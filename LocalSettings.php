55e0e9cdbbeeaaf0420f9e537d38904735596f53
Leo Proechel <leoproechel@Leos-big-mac.fios-router.home>
2020-05-17 23:45:16 -0400

    troubleshooting settings

--- LocalSettings.php
+++ LocalSettings.php
@@ -19,7 +19,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 ## Uncomment this to disable output compression
 # $wgDisableOutputCompression = true;
 
-$wgSitename = "NGL Communities Wiki";
+$wgSitename = "NGL Communities";
 $wgMetaNamespace = "NGL_Communities";
 
 ## The URL base path to the directory containing the wiki;
