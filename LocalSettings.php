cf69be50373d15dadb40ca9da825ef5663127a1e
Leo Proechel <leoproechel@Leos-big-mac.local>
2020-05-17 20:49:41 -0400

    troubleshoot settins file

--- LocalSettings.php
+++ LocalSettings.php
@@ -116,7 +116,7 @@ $wgDiff3 = "/usr/bin/diff3";
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
 
-$wgDefaultSkin = "vector";
+// $wgDefaultSkin = "vector";
 $wgStylePath        = "$wgScriptPath/skins";
 
 wfLoadExtension( 'MobileFrontend' );
