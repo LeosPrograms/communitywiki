aaa883b3e10f6ee1bab60c3137c024df86b5800a
Leo Proechel <leoproechel@Leos-big-mac.local>
2020-05-17 21:24:01 -0400

    commit

--- LocalSettings.php
+++ LocalSettings.php
@@ -116,10 +116,9 @@ $wgDiff3 = "/usr/bin/diff3";
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
 
-$wgDefaultSkin = "vector";
-$wgStylePath        = "$wgScriptPath/skins";
+// $wgDefaultSkin = "vector";
 
-wfLoadExtension( 'MobileFrontend' );
+// wfLoadExtension( 'MobileFrontend' );
 wfLoadSkin( 'MinervaNeue' );
 
 
