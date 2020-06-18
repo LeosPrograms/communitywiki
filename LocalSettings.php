c7c5cb0a364271abfd45dcb2b05e8d0546836912
Leo Proechel <leoproechel@Leos-big-mac.fios-router.home>
2020-06-18 15:35:03 -0400

    try new skin

--- LocalSettings.php
+++ LocalSettings.php
@@ -116,7 +116,7 @@ $wgDiff3 = "/usr/bin/diff3";
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
 
-$wgDefaultSkin = "Timeless";
+$wgDefaultSkin = "Amethyst";
 
 # End of automatically generated settings.
 # Add more configuration options below.
