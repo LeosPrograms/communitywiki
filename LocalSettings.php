39539d5332a5fe888c5cc24070e7388039949e78
Leo Proechel <leoproechel@Leos-big-mac.fios-router.home>
2020-06-18 15:20:24 -0400

    try new skin

--- LocalSettings.php
+++ LocalSettings.php
@@ -116,11 +116,11 @@ $wgDiff3 = "/usr/bin/diff3";
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
 
-$wgDefaultSkin = "MinervaNeue";
+$wgDefaultSkin = "Timeless";
 
 # End of automatically generated settings.
 # Add more configuration options below.
 
 wfLoadExtension( 'MobileFrontend' );
-wfLoadSkin( 'MinervaNeue' );
+wfLoadSkin( 'minervaneue' );
 $wgMFAutodetectMobileView = true;
